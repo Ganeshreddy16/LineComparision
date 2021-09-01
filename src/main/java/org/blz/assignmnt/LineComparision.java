b17a058861d97bd9b6cc2a1bb61757a723e0f867
Ganesh Reddy <reddyganesh125@gmail.com>
2021-09-01 21:12:50 +0530

    Added:Equals Method

--- src/main/java/org/blz/assignmnt/LineComparision.java
+++ src/main/java/org/blz/assignmnt/LineComparision.java
@@ -7,14 +7,20 @@ public class LineComparision
     {
         Scanner input = new Scanner(System.in);
         int x1, x2, y1, y2;
+        String Length1,Length2;
         System.out.println("Enter Coordinates x1,y1,x2,y2");
         x1 = input.nextInt();
         y1 = input.nextInt();
         x2 = input.nextInt();
         y2 = input.nextInt();
-        double Length =Math.sqrt(Math.pow( (x2-x1) ,2 ) + Math.pow( (y2-y1) , 2) );
-        System.out.println(Length);
-        System.out.println("Your length is " + Length);
+        Length1 =String.valueOf(Math.sqrt(Math.pow( (x2-x1) ,2 ) + Math.pow( (y2-y1) , 2) ));
+        System.out.println(Length1);
+        System.out.println("Your length is " + Length1);
+        Length2 =String.valueOf(Math.sqrt(Math.pow( (x2-x1) ,2 ) + Math.pow( (y2-y1) , 2) ));
+        System.out.println(Length2);
+        System.out.println("Your length is " + Length2);
+
+        System.out.println(Length1.equals(Length2));
     }
 
 }
